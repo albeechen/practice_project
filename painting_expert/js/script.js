d8989e9187d6df2d1ed0d0a977c52d06427e5c78
albeechen <linda3632000@hotmail.com>
2017-07-27 23:20:55 -0700

    create painging expert project

---
+++ painting_expert/js/script.js
@@ -0,0 +1,11 @@
+var img = document.getElementById("original");
+var c = document.getElementById("copy");
+var ctx = c.getContext("2d");
+c.onmousemove=function(e) {
+	var line_width = 50;
+	var x = e.clientX;
+	var y = e.clientY;
+	var rx = x-751;
+	var ry = y-200;
+	ctx.drawImage(img, rx, ry, line_width, line_width, rx, ry, line_width, line_width);
+}
