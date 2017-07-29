3dbc3d4c0d0851b2faa2b1340c421acc345d9971
albeechen <linda3632000@hotmail.com>
2017-07-28 22:43:03 -0700

    move html attribute to js and change cursor icon

--- painting_expert/js/script.js
+++ painting_expert/js/script.js
@@ -1,16 +1,19 @@
 var img = document.getElementById("original");
+img.setAttribute("src", "source/photo.jpg");
 var c = document.getElementById("copy");
+c.setAttribute("width", "500");
+c.setAttribute("height", "330");
 var ctx = c.getContext("2d");
-c.onmousemove=function(e) {
-	var line_width = 50;
-	var x = e.clientX;
-	var y = e.clientY;
-	var rx = x-751;
-	var ry = y-200;
-	ctx.drawImage(img, rx, ry, line_width, line_width, rx, ry, line_width, line_width);
+c.onmousemove = function(e) {
+    var line_width = 35;
+    var x = e.clientX;
+    var y = e.clientY;
+    var rx = x - 760;
+    var ry = y - 180;
+    ctx.drawImage(img, rx, ry, line_width, line_width, rx, ry, line_width, line_width);
 }
 
 var btn = document.getElementById("restartBtn");
-btn.onclick = function(){
-	location.reload();
+btn.onclick = function() {
+    location.reload();
 }
